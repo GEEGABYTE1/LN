b4246b721afccd223cf4d6ea111788361f13d8e1
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-11-12 17:37:55 -0500

    Global Chat

--- localnetwork.py
+++ localnetwork.py
@@ -4,15 +4,38 @@ from pymongo import MongoClient
 from termcolor import colored
 
 class Network:
+    cluster = MongoClient("mongodb+srv://GEEGABYTE1:12345@socialmedia.few6z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    db = cluster['socialmedia']['messaging']
+    all_messages = db.find({})
     
     
 
     def sending_message(self):
-        pass
+        while True:
+            date = datetime.now().strftime("%x")
+            for message in self.all_messages:
+                try:
+                    if date != message['Date']:
+                        print(colored('Today: {}'.format(message['Time']), 'red'))
+                    else:
+                        print(colored("{} ~ {}".format(message['Date'], message['Time']), "red"))
+                    print(colored("From: ", 'green'), message['Id'])
+                    print(colored("Message: ", 'green'), message['Message'])
+                    print('-'*25)
+                except:
+                    pass
+            
+            person = input("Name: ")
+            message = input("Message: ")
 
+            if message == '/quit':
+                break
+            else:
+                time = datetime.now().strftime("%X")
+                msg = {"Id": person, "Message": message, "Date": date, "Time":time}
+                self.db.insert_one(msg)
+                print('-'*25)
         
 
 
-test_network = Network()
-
-print(test_network.sending_message())
+network = Network()
