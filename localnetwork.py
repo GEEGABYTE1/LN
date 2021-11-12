d6ea092da6f7f0d820adb86906385a0796f01a15
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-11-11 22:13:48 -0500

    DB Error

--- localnetwork.py
+++ localnetwork.py
@@ -1,8 +1,18 @@
-import datetime
+from datetime import datetime
 import time
 from pymongo import MongoClient
 from termcolor import colored
 
 class Network:
-    pass 
+    
+    
 
+    def sending_message(self):
+        pass
+
+        
+
+
+test_network = Network()
+
+print(test_network.sending_message())
