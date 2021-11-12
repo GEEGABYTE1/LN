4ed01d51982537fcdcff3cc4209d43816404672a
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-11-12 17:38:20 -0500

    Initial Homepage

---
+++ homepage.py
@@ -0,0 +1,19 @@
+import time 
+from localnetwork import network
+
+
+
+class Homepage:
+
+    def landing(self):
+        print("Welcome to LN")
+        time.sleep(0.2)
+        print("Short form for LocalNetwork")
+        time.sleep(0.2)
+        print("/global_chat: To acess the global chat worldwide")
+        
+        while True:
+            user_prompt = str(input(': '))
+
+            
+        
